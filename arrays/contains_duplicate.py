0c899b7956657611b5881ff64773b18b5f07d295
Asfar Khan <asfarkhan9595@gmail.com>
2026-06-13 20:22:49 +0530

    Day 2 - Contains Duplicate

---
+++ arrays/contains_duplicate.py
@@ -0,0 +1,16 @@
+# Day 2 - Contains Duplicate
+# LeetCode #217
+# Given array, return True if any value appears twice
+
+def contains_duplicate(nums):
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+# Test
+print(contains_duplicate([1, 2, 3, 1]))   # True
+print(contains_duplicate([1, 2, 3, 4]))   # False
+print(contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))  # True
