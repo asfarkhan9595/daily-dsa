d679d9789e86300b5e26b06f7e696e0554e12feb
Asfar Khan <asfarkhan9595@gmail.com>
2026-06-13 20:19:08 +0530

    done

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,5 +1,5 @@
 # Day 1 - Two Sum
-# LeetCode #1
+# LeetCode #1 
 
 def two_sum(nums, target):
     seen = {}
@@ -8,8 +8,8 @@ def two_sum(nums, target):
         if complement in seen:
             return[seen[complement], i]
         seen[num] = i
-    return []
+    return [] 
 
-#Test 
+#Test  
 print(two_sum([2, 7, 11, 15],9)) #[0,1]
 print(two_sum([3, 2, 4],6)) #[1, 2]
