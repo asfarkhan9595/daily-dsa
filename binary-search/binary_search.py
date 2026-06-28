16826b32e792cdefd0b3007a5b12e8096de651d4
Asfar Khan <asfarkhan9595@gmail.com>
2026-06-28 23:32:44 +0530

    Day 16 - Binary Search

---
+++ binary-search/binary_search.py
@@ -0,0 +1,22 @@
+# Day 16 - Binary Search
+# LeetCode #704
+# Sorted array mein target dhundo
+
+def binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+# Test
+print(binary_search([-1,0,3,5,9,12], 9))   # 4
+print(binary_search([-1,0,3,5,9,12], 2))   # -1
+print(binary_search([5], 5))               # 0 
